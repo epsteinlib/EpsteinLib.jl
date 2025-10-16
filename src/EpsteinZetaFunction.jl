dcabe105f9e11ffdd5e1916323fdea086231ad86
David Gomez-Castro <david@gomezcastro.xyz>
2025-10-16 16:42:26 +0200

    Cleaning up ccall

--- src/EpsteinZetaFunction.jl
+++ src/EpsteinZetaFunction.jl
@@ -10,8 +10,6 @@ if occursin("arm64-apple-darwin", Sys.MACHINE)
     
     using Libdl
     libepstein_path = joinpath(@__DIR__, "lib", "libepstein.dylib")
-    libepstein = Libdl.dlopen(libepstein_path)
-
     """
     Calls the C function `epsteinZeta` from the shared library.
         double complex epsteinZeta(double nu, unsigned int dim, const double *A, const double *x, const double *y);
@@ -22,9 +20,6 @@ if occursin("arm64-apple-darwin", Sys.MACHINE)
         dim = Base.UInt32(size(A, 1))
         return @ccall libepstein_path.epsteinZeta(nu::Float64, dim::UInt32, A::Ref{Float64}, x::Ref{Float64}, y::Ptr{Float64})::Complex{Float64}
     end
-
-    Libdl.dlclose(libepstein)
-
 else
     error("Unsupported architecture: $(Sys.MACHINE). This library currently supports only arm64-apple-darwin. Waiting for the jll package to appear. Because of a linker error the jll does not support arm64-apple-darwin.")
 end
