ba9284bdb3c0629bc8ac5eb90160b272ca40b9f1
David Gomez-Castro <david@gomezcastro.xyz>
2025-10-22 15:24:13 +0200

    allow calls specifying d,x,y or none of them

--- src/EpsteinZetaFunction.jl
+++ src/EpsteinZetaFunction.jl
@@ -5,9 +5,10 @@ using Epsteinlib_jll, LinearAlgebra
 export epsteinzeta
 
 """
+    epsteinzeta(ν::Float64,A::Matrix{Float64},x::Vector{Float64},y::Vector{Float64})
 Calls the C function `epsteinZeta` from the shared library.
     double complex epsteinZeta(double nu, unsigned int dim, const double *A, const double *x, const double *y);
-Approximatess
+Approximates
 ``Z_{\\nu, A}(x, y) = \\sum_{z \\in A \\mathbb{Z}^d, z \\ne x} \frac{-e^{2\\pi i y \\cdot z}}{|x-z|^\\nu}``
 if the real part of nu is greater than the system dimension, and the meromorphic continuation otherwise.
 """
@@ -29,25 +30,60 @@ function epsteinzeta(
 end
 
 """
-Approximates
-``Z_{\nu, \\mathbb Z^d}(x, 0) = \\sum_{z \\in \\mathbb Z^d, z \\ne x} |x-z|^{-\\nu}``
+
+    epsteinzeta(ν; d, x, y, A)
+where d, x, y, A are optional. x and y default to zero of size d, and A to the identity matrix of size d.  
+
+Approximatess
+``Z_{\\nu, A}(x, y) = \\sum_{z \\in A \\mathbb{Z}^d, z \\ne x} \frac{-e^{2\\pi i y \\cdot z}}{|x-z|^\\nu}``
 if the real part of nu is greater than the system dimension, and the meromorphic continuation otherwise.
-"""
-function epsteinzeta(ν::Float64, x::Vector{Float64})::Complex{Float64}
-    A = Matrix{Float64}(I, length(x), length(x))
-    y = zeros(Float64, length(x))
-    return epsteinzeta(ν, A, x, y)
-end
 
 """
-Approximates
-``Z_{\nu, \\mathbb Z^d}(0, 0) = \\sum_{z \\in \\mathbb Z^d, z \\ne 0} |z|^{-\\nu}``
-if the real part of nu is greater than the system dimension, and the meromorphic continuation otherwise.
-"""
-function epsteinzeta(ν::Float64, d::Int64)::Complex{Float64}
-    x = zeros(Float64, d)
-    A = Matrix{Float64}(I, d, d)
-    y = zeros(Float64, d)
+function epsteinzeta(
+    ν::Real;
+    d::Union{Integer,Nothing} = nothing,
+    x::Union{Vector{Real},Nothing} = nothing,
+    y::Union{Vector{Real},Nothing} = nothing,
+    A::Union{Matrix{Real},Nothing} = nothing,
+)::Complex{Float64}
+    if x==nothing && y==nothing && d==nothing
+        throw(ArgumentError("Either d, x, or y must be specified"))
+    end
+    if d==nothing
+        if x==nothing
+            d = length(y)
+            x = zeros(d)
+        else
+            d = length(x)
+            if y==nothing
+                y = zeros(d)
+            elseif length(y) != d
+                throw(ArgumentError("x and y must be the same length"))
+            end
+        end
+    else
+        if x == nothing
+            x = zeros(d)
+        elseif length(x) != d
+            throw(ArgumentError("x must be of length d"))
+        end
+        if y == nothing
+            y = zeros(d)
+        elseif length(y) != d
+            throw(ArgumentError("y must be of length d"))
+        end
+    end
+    x = convert(Vector{Float64}, x)
+    y = convert(Vector{Float64}, y)
+
+    if A == nothing
+        A = Matrix{Float64}(I, d, d)
+    elseif size(A) != (d, d)
+        throw(ArgumentError("Dimensions mismatch at A"))
+    else
+        A = convert(Matrix{Float64}, A)
+    end
+
     return epsteinzeta(ν, A, x, y)
 end
 
--- test/epstein_tests.jl
+++ test/epstein_tests.jl
@@ -6,32 +6,50 @@ using SpecialFunctions
 @testset "Matches Riemann zeta at d=1" begin
     d = 1
     for ν = 2.0:0.5:5.0
-        @test epsteinzeta(ν, d) ≈ 2 * zeta(ν) atol = 1e-6
+        ref = 2 * zeta(ν)
+        @test epsteinzeta(ν; d = 1) ≈ ref atol = 1e-6
+        @test epsteinzeta(ν; x = [0.0]) ≈ ref atol = 1e-6
+        @test epsteinzeta(ν; d = 1, x = [0.0]) ≈ ref atol = 1e-6
+        @test epsteinzeta(ν; y = [0.0]) ≈ ref atol = 1e-6
+        @test epsteinzeta(ν; d = 1, y = [0.0]) ≈ ref atol = 1e-6
+        @test epsteinzeta(ν; x = [0.0], y = [0.0]) ≈ ref atol = 1e-6
+        @test epsteinzeta(ν; d = 1, x = [0.0], y = [0.0]) ≈ ref atol = 1e-6
     end
+end
+
+@testset "Test errors" begin
+    ν = 2.0
+    @test_throws ArgumentError epsteinzeta(ν)
+    @test_throws ArgumentError epsteinzeta(ν; d = 1, x = [0.0, 0.0])
+    @test_throws ArgumentError epsteinzeta(ν; d = 1, y = [0.0, 0.0])
+    @test_throws ArgumentError epsteinzeta(ν; x = [0.0], y = [0.0, 0.0])
+    @test_throws ArgumentError epsteinzeta(ν; d = 1, x = [0.0, 0.0], y = [0.0, 0.0])
 end
 
 @testset "Compare single non-diagonal evaluation with reference value obtained from the C implementation" begin
 
     nu = 1/2
-    A = [1 1/2;
-         0 sqrt(3)/2]
+    A = [
+        1 1/2;
+        0 sqrt(3)/2
+    ]
     x = [1/10, 2/10]
     y = [3/10, 4/10]
 
     ref = 0.8819439608604308 - 0.10322404491724824im
 
-    @test epsteinzeta(nu,A,x,y) ≈ ref atol = 1e-16
+    @test epsteinzeta(nu, A, x, y) ≈ ref atol = 1e-16
 end
 
 
 truncated_power(ν, s) = abs(s) > 1e-10 ? abs(s)^(-ν) : 0.0
-sum_vertices(ν, N) = sum(truncated_power(ν, sqrt(x^2 + y^2)) for x = -N:N, y = -N:N)
+sum_vertices(ν, N) = sum(truncated_power(ν, sqrt(x^2 + y^2)) for x = (-N):N, y = (-N):N)
 
 @testset "Matches sum at d=2" begin
     d = 2
     N = 5_000
     for ν = 3.0:1.0:5.0
-        @test epsteinzeta(ν, d) ≈ sum_vertices(ν, N) atol = 1e-2
+        @test epsteinzeta(ν; d = d) ≈ sum_vertices(ν, N) atol = 1e-2
     end
 end
 
@@ -40,7 +58,7 @@ end
     ν = 2.5
     x = [0.1, 0.2]
 
-    z1 = epsteinzeta(ν, x)
+    z1 = epsteinzeta(ν; x = x)
     @test isa(z1, Complex{Float64})
 
     A = Matrix{Float64}(I, length(x), length(x))
@@ -49,7 +67,7 @@ end
     @test z1 == expected
 
     d = 3
-    z2 = epsteinzeta(ν, d)
+    z2 = epsteinzeta(ν; d = d)
     @test isa(z2, Complex{Float64})
 
     A2 = Matrix{Float64}(I, d, d)
@@ -57,6 +75,7 @@ end
     y2 = zeros(Float64, d)
     expected2 = epsteinzeta(ν, A2, x2, y2)
     @test z2 == expected2
+
 end
 
 
