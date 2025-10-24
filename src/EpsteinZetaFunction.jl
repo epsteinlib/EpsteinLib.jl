a44b8bb3cfd4bdb03ba2ba85bc6d5404d7e12332
David Gómez-Castro <david@gomezcastro.xyz>
2025-10-24 15:09:10 +0200

    Refactor epsteinzeta function using clean arguments

--- src/EpsteinZetaFunction.jl
+++ src/EpsteinZetaFunction.jl
@@ -29,23 +29,8 @@ function epsteinzeta(
     )::Complex{Float64}
 end
 
-"""
-
-    epsteinzeta(ν; d, x, y, A)
-where d, x, y, A are optional. x and y default to zero of size d, and A to the identity matrix of size d.  
-
-Approximatess
-``Z_{\\nu, A}(x, y) = \\sum_{z \\in A \\mathbb{Z}^d, z \\ne x} \frac{-e^{2\\pi i y \\cdot z}}{|x-z|^\\nu}``
-if the real part of nu is greater than the system dimension, and the meromorphic continuation otherwise.
-
-"""
-function epsteinzeta(
-    ν::T0;
-    d::Union{Integer,Nothing} = nothing,
-    x::Union{Vector{T1},Nothing} = nothing,
-    y::Union{Vector{T2},Nothing} = nothing,
-    A::Union{Matrix{T3},Nothing} = nothing,
-)::Complex{Float64} where {T0<:Real,T1<:Real,T2<:Real,T3<:Real}
+function cleanuparguments(ν, A, x, y)
+    ν = convert(Float64, ν)
     if x==nothing && y==nothing && d==nothing && A==nothing
         throw(ArgumentError("Either d, x, y, or A must be specified"))
     end
@@ -60,6 +45,7 @@ function epsteinzeta(
             end
         end
     end
+    d = convert(Int64, d)
 
     if x == nothing
         x = zeros(d)
@@ -87,6 +73,28 @@ function epsteinzeta(
     else
         A = convert(Matrix{Float64}, A)
     end
+
+    return (ν, A, x, y)
+end
+
+"""
+
+    epsteinzeta(ν; d, x, y, A)
+where d, x, y, A are optional. x and y default to zero of size d, and A to the identity matrix of size d.  
+
+Approximatess
+``Z_{\\nu, A}(x, y) = \\sum_{z \\in A \\mathbb{Z}^d, z \\ne x} \frac{-e^{2\\pi i y \\cdot z}}{|x-z|^\\nu}``
+if the real part of nu is greater than the system dimension, and the meromorphic continuation otherwise.
+
+"""
+function epsteinzeta(
+    ν::T0;
+    d::Union{Integer,Nothing} = nothing,
+    x::Union{Vector{T1},Nothing} = nothing,
+    y::Union{Vector{T2},Nothing} = nothing,
+    A::Union{Matrix{T3},Nothing} = nothing,
+)::Complex{Float64} where {T0<:Real,T1<:Real,T2<:Real,T3<:Real}
+    ν, A, x, y = cleanuparguments(ν, A, x, y)
 
     return epsteinzeta(ν, A, x, y)
 end
@@ -129,47 +137,7 @@ function epsteinzetareg(
     y::Union{Vector{T2},Nothing} = nothing,
     A::Union{Matrix{T3},Nothing} = nothing,
 )::Complex{Float64} where {T0<:Real,T1<:Real,T2<:Real,T3<:Real}
-    if x==nothing && y==nothing && d==nothing && A==nothing
-        throw(ArgumentError("Either d, x, y, or A must be specified"))
-    end
-    if d==nothing
-        if x!=nothing
-            d = length(x)
-        else
-            if y!=nothing
-                d = length(y)
-            else
-                d = size(A, 1)
-            end
-        end
-    end
-
-    if x == nothing
-        x = zeros(d)
-    else
-        if length(x)==d
-            x = convert(Vector{Float64}, x)
-        else
-            throw(ArgumentError("Incompatible size for x"))
-        end
-    end
-    if y == nothing
-        y = zeros(d)
-    else
-        if length(y)==d
-            y = convert(Vector{Float64}, y)
-        else
-            throw(ArgumentError("Incompatible size for y"))
-        end
-    end
-
-    if A == nothing
-        A = Matrix{Float64}(I, d, d)
-    elseif size(A) != (d, d)
-        throw(ArgumentError("Incompatible size of A"))
-    else
-        A = convert(Matrix{Float64}, A)
-    end
+    ν, A, x, y = cleanuparguments(ν, A, x, y)
 
     return epsteinzetareg(ν, A, x, y)
 end
