604341051a73f93f256e9b6efe3046980494ef45
David Gomez-Castro <david@gomezcastro.xyz>
2025-10-24 14:35:38 +0200

    automatic conversion of d and nu to correct types

--- src/EpsteinZetaFunction.jl
+++ src/EpsteinZetaFunction.jl
@@ -46,6 +46,7 @@ function epsteinzeta(
     y::Union{Vector{T2},Nothing} = nothing,
     A::Union{Matrix{T3},Nothing} = nothing,
 )::Complex{Float64} where {T0<:Real,T1<:Real,T2<:Real,T3<:Real}
+    ν = convert(Float64, ν)
     if x==nothing && y==nothing && d==nothing && A==nothing
         throw(ArgumentError("Either d, x, y, or A must be specified"))
     end
@@ -60,6 +61,7 @@ function epsteinzeta(
             end
         end
     end
+    d = convert(Int64, d)
 
     if x == nothing
         x = zeros(d)
